729749f4a326155f4d365b66785dae535148a4a4
AGUPTA <a.gupta@devon.nl>
2023-01-03 23:09:29 +0530

    added library infos inside webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,48 @@
 const path = require("path");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const pkg = require("./package.json");
+
+const libraryName = pkg.name;
 
 module.exports = {
-  entry: "./index.js",
+  entry: path.join(__dirname, "./index.js"),
   mode: "development",
   output: {
     filename: "bundle.js",
+    path: path.join(__dirname, "./dist"),
+    library: libraryName,
+    libraryTarget: "umd",
+    publicPath: "/dist/",
+    umdNamedDefine: true,
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+      assets: path.resolve(__dirname, "assets"),
+    },
   },
   devtool: "inline-source-map",
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "react-tooltip.css",
+    }),
+  ],
   module: {
     rules: [
       {
@@ -17,7 +52,7 @@ module.exports = {
       },
       {
         test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
